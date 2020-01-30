52c95c87f4f6f277766a6604367c6e0f1a85b2c1
max <maxjunhuixu@gmail.com>
2020-01-30 17:27:08 +0800

    refactor

--- public/index.js
+++ public/index.js
@@ -1,41 +1,17 @@
 let slider = document.getElementById("ballSize")
 let score = document.getElementById("ballSizeVal")
-score.innerHTML = '0'
-
 let bubbleArea = document.querySelector(".bubble-area")
+let start = document.querySelector("#startButton")
+
 let height = bubbleArea.clientHeight
 let time = height / slider.value
 let i = 0
 let j = 0
-let start = document.querySelector("#startButton")
 let intervalId
-
-slider.oninput = function () {
-  time = height / this.value
-}
+score.innerHTML = '0'
 
 function getRandomVal(maximum, minimum) {
   return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
-}
-
-start.onclick = function (e) {
-  start.innerHTML = e.target.innerHTML === "Start" ? "Pause" : "Start"
-
-  if (start.innerHTML === "Pause") {
-    // when game is started
-    intervalId = gameMode()
-    document.querySelectorAll(".bb").forEach(e => {
-      e.style.animationIterationCount = infinite
-      e.style.opacity = 1
-    })
-  } else {
-    // when game is paused
-    clearInterval(intervalId)
-    document.querySelectorAll(".bb").forEach(e => {
-      e.style.animationIterationCount = "inherit"
-      e.style.opacity = 0
-    })
-  }
 }
 
 function gameMode() {
@@ -71,4 +47,28 @@ function gameMode() {
       j = 0
     }
   }, 1000)
+}
+
+slider.oninput = function () {
+  time = height / this.value
+}
+
+start.onclick = function (e) {
+  start.innerHTML = e.target.innerHTML === "Start" ? "Pause" : "Start"
+
+  if (start.innerHTML === "Pause") {
+    // when game is started
+    intervalId = gameMode()
+    document.querySelectorAll(".bb").forEach(e => {
+      e.style.animationIterationCount = 'infinite'
+      e.style.opacity = 1
+    })
+  } else {
+    // when game is paused
+    clearInterval(intervalId)
+    document.querySelectorAll(".bb").forEach(e => {
+      e.style.animationIterationCount = "inherit"
+      e.style.opacity = 0
+    })
+  }
 }
