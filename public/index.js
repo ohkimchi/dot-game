8b962af311d854897fb1062be5b94201bbb2cdfb
eonni <maxjunhuixu@gmail.com>
2020-01-30 15:57:05 +0800

    Todo: to stop the inifite animation after pausing

--- public/index.js
+++ public/index.js
@@ -3,7 +3,7 @@ var size = document.getElementById('ballSizeVal')
 size.innerHTML = slider.value
 
 let bubbleArea = document.querySelector('.bubble-area')
-let height = 80
+let height = bubbleArea.clientHeight
 let time = height / size.innerHTML
 let i = 0;
 let j = 0
@@ -13,25 +13,52 @@ slider.oninput = function () {
   time = height / this.value
 }
 
+function getRandomVal(maximum, minimum) {
+  return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
+}
 
-setInterval(() => {
-  let horizontalPosition = Math.floor(Math.random() * 95) + 'vw';
-  console.log(horizontalPosition, time)
+let start = document.querySelector('#startButton')
+let intervalId
+start.onclick = function (e) {
+  start.innerHTML = e.target.innerHTML === 'Start' ? 'Pause' : 'Start'
+  // let intervalId = gameMode()
 
-  if (i < 10) {
-    let newBall = document.createElement('DIV')
-    newBall.className = "bb"
-    newBall.id = `bb-${i}`
-    newBall.style.animationDuration = time + 's'
-    newBall.style.left = horizontalPosition
-    document.querySelector('.bubble-area').appendChild(newBall)
-    i++
+  if (start.innerHTML === 'Pause') {
+    intervalId = gameMode()
+  } else {
+    clearInterval(intervalId)
   }
-  if (j < 10) {
-    document.querySelector(`#bb-${j}`).style.left = horizontalPosition
-  }
-  j++
-  if (j == 10) {
-    j = 0
-  }
-}, 1000)
+}
+
+function gameMode() {
+  return setInterval(() => {
+    let horizontalPosition = getRandomVal(100, 10) + 'vw';
+    let diameter = getRandomVal(100, 10)
+    let i = document.querySelectorAll('.bb').length
+
+    if (i < 60) {
+      let newBall = document.createElement('SPAN')
+      newBall.className = "bb"
+      newBall.id = `bb-${i}`
+      newBall.style.animationDuration = time + 's'
+      newBall.style.left = horizontalPosition
+      newBall.style.width = diameter + 'px'
+      newBall.style.height = diameter + 'px'
+      newBall.value = diameter
+      bubbleArea.appendChild(newBall)
+      newBall.onclick = function (e) {
+        let score = 100 - e.target.value
+        size.innerHTML = score + Number(size.innerHTML)
+        bubbleArea.removeChild(e.target)
+      }
+      i++
+    }
+    if (j < 60) {
+      document.querySelector(`#bb-${j}`).style.left = horizontalPosition
+    }
+    j++
+    if (j == 60) {
+      j = 0
+    }
+  }, 1000)
+}
